8bd99add99a071295b1338a5bde88e6013e3e51c
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2025-01-27 17:36:20 +0530

    fixed be-fe link issue

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['*', 'http://localhost:3000', 'http://localhost:3001'], // Allow requests from this origin
+    origin: [
+      '*',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      `https://recalll.vercel.app`,
+    ], // Allow requests from this origin
     credentials: true, // Allow cookies and credentials
   })
 )
